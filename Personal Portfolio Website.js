5c3d72b2911ba90427027bbd8cb9f065120ee235
mmothiba4 <mmothiba4@gmail.com>
2023-10-20 11:09:20 +0200

    capstone

--- Personal Portfolio Website.js
+++ Personal Portfolio Website.js
@@ -31,53 +31,6 @@ document.getElementById("emailForm").addEventListener("submit", function (event)
     }
 });
 
-// Function to validate the password
-function validatePassword() {
-    const password = document.getElementById("password").value;
-    const errorElement = document.getElementById("error");
-
-    // Define regular expressions for each validation rule
-    const lengthRegex = /.{8,}/;
-    const uppercaseRegex = /[A-Z]/;
-    const lowercaseRegex = /[a-z]/;
-    const digitRegex = /[0-9]/;
-    const specialCharRegex = /[!@#$%^&*]/;
-
-    // Check each rule and display error messages as needed
-    if (!lengthRegex.test(password)) {
-        errorElement.textContent = "Password must be at least 8 characters long.";
-        return false;
-    }
-    if (!uppercaseRegex.test(password)) {
-        errorElement.textContent = "Password must contain at least one uppercase letter.";
-        return false;
-    }
-    if (!lowercaseRegex.test(password)) {
-        errorElement.textContent = "Password must contain at least one lowercase letter.";
-        return false;
-    }
-    if (!digitRegex.test(password)) {
-        errorElement.textContent = "Password must contain at least one numeric digit.";
-        return false;
-    }
-    if (!specialCharRegex.test(password)) {
-        errorElement.textContent = "Password must contain at least one special character (e.g., !@#$%^&*).";
-        return false;
-    }
-
-    // Clear any previous error messages and allow form submission
-    errorElement.textContent = "";
-    return true;
-}
-
-// Add an event listener to the form to call validatePassword on submit
-document.getElementById("passwordForm").addEventListener("submit", function (event) {
-    if (!validatePassword()) {
-        event.preventDefault(); // Prevent form submission if validation fails
-    }
-});
-
-
 // Function to handle form submission and perform validation
 function handleFormSubmit(event) {
     event.preventDefault(); // Prevent default form submission
@@ -108,8 +61,7 @@ function handleFormSubmit(event) {
 // Add an event listener to the form to handle form submission
 document.getElementById("submitForm").addEventListener("submit", handleFormSubmit);
 
-
-<?php
+//<?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $name = strip_tags(trim($_POST["name"]));
     $email = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
